1c92e88b626a55e1faeb488094d8efd25ef60d8a
Gabrielnero000 <gabriel.leite083@gmail.com>
2017-04-28 14:55:36 -0300

    Classe com os principais dados do projeto.

--- src/Produto/Produto.java
+++ src/Produto/Produto.java
@@ -1,6 +1,29 @@
 package Produto;
 
 public class Produto {
+    
+    private static final String SEC_1 = "Orgânicos";
+    private static final String SEC_2 = "Bebida";
+    private static final String SEC_3 = "Mercearia";
+    private static final String SEC_4 = "Mercearia Doce";
+    private static final String SEC_5 = "Limpeza";
+    private static final String SEC_6 = "Higiene e Perfumaria";
+    private static final String SEC_7 = "Carnes";
+    private static final String SEC_8 = "Feira";
+    private static final String SEC_9 = "Frios";
+    private static final String SEC_10 = "Orgânicos";
+    private static final String SEC_11 = "Orgânicos";
+    private static final String SEC_12 = "Orgânicos";
+    
+    
     private int codigo;
+    private String nome;
+    private String secao;
+    private int t_secao;
+    private String unidade;
+    private int quantidade;
+    private double p_custo;
+    private double p_venda;
+    
     
 }
